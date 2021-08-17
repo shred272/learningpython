201c0fc8fb783cbe8b39419cd4abb8424684280d
Sam Chengli <samchengli@gmail.com>
2021-08-16 22:19:41 -0400

    added comments and formatted

--- ex36.py
+++ ex36.py
@@ -2,26 +2,27 @@
 HaveMeat = False
 HaveMoney = False
 
+#the game was designed with no real way to lose so you will loop until you beat it or Ctrl + C to end...
+
 def outside():
-    print("You are drunk from partying and you are standing outside in a sandy plaza on Tatooine.")
     print("Ahead of you, you see the notorious Mos Eisly Cantina bar where your favourite band plays.")
     print("On your right, there is a local kebab shop.\n")
-
     print("Where would you like to go?")
 
     choice = input("(bar/kebab)> ")
 
     if choice == "bar":
-        print("You head to the Mos Eisley Cantina.")
+        print("You head to the Mos Eisley Cantina.\n")
         cantina()
     elif choice == "kebab":
-        print("You head to the kebab shop.")
+        print("You head to the kebab shop.\n")
         kebab_shop()
     else:
         print("You can't make up your mind and drunkenly stumble around for a bit.\n")
         outside()
 
 def kebab_shop():
+#   need to pull the global variables otherwise the variables are local and are undefined
     global HaveMeat
     global HaveMoney
     print("You enter the kebab shop where Gromodo greets you with a grunt.\n")
@@ -37,7 +38,8 @@ def kebab_shop():
             HaveMoney = True
         else:
             print("You let go a fart to conceal the meat smell and smile at Gromodo.\n")
-#   we have 2 IF statements so both can be processed individually
+#   we have 2 "if" statements so both can be processed individually
+#   (so you can waste your money right away)
     if HaveMoney:
         print("Do you want to buy kebab from Gromodo?")
 
@@ -48,40 +50,41 @@ def kebab_shop():
 
             print("You hand over your money and Gromodo gives you some kebab.")
             print('"THIS IS THE BEST KEBAB EVER!" You drunkenly exclaim as you finish your food.')
-            print("You leave the shop.")
+            print("You leave the shop.\n")
             outside()
         else:
             print("Gromodo yells at you to leave if you won't buy anything.")
-            print("You leave the shop.")
+            print("You leave the shop.\n")
             outside() 
     else:
-        print("You are too drunk and your head is spinning. You go back outside.")
+        print("You are too drunk and your head is spinning. You go back outside.\n")
         outside()
 
 def cantina():
     print("You enter the Mos Eisley Cantina and your favourite band is playing your favourite song!")
     print("What a coincidence!")
     print("You drunkenly dance for a bit and notice some swinging doors to the kitchen and at the other end of the room you see a door to going backstage.")
-    print("Where do you decide to go?")
+    print("Where do you decide to go?\n")
     choice = input("(outside/backstage/kitchen)> ")
 
     if choice == "outside":
-        print("You leave the building.")
+        print("You leave the building.\n")
         outside()
     elif choice == "backstage":
-        print("You head to the door that leads backstage.")
+        print("You head to the door that leads backstage.\n")
         backstage()
     elif choice == "kitchen":
-        print("You head through the swinging doors that lead into the kitchen.")
+        print("You head through the swinging doors that lead into the kitchen.\n")
         kitchen()
     else:
-        print("You are too drunk and get thrown out of the building.")
+        print("You are too drunk and get thrown out of the building.\n")
         outside()
 
 def backstage():
+#   need to pull the global variable otherwise variable is local and is undefined
     global HaveMoney
     print("You enter a corridor and can see the entrance to go backstage but it is blocked by a large bouncer.")
-    print("The bouncer sees you and tells you to get lost.")
+    print("The bouncer sees you and tells you to get lost.\n")
 
     if HaveMoney:
         print("Do you bribe the bouncer with the large pouch of peggats?")
@@ -91,13 +94,14 @@ def backstage():
             print("CONGRATULATIONS! You got to meet your favourite band and have some drinks with them!")
             exit(0)
         else:
-            print("You leave the corridor and go back in the bar.")
+            print("You leave the corridor and go back in the bar.\n")
             cantina()
     else:
-        print("You leave the corridor and go back in the bar.")
+        print("You leave the corridor and go back in the bar.\n")
         cantina()
 
 def kitchen():
+#   need to pull the global variable otherwise variable is local and is undefined
     global HaveMeat
     print("You enter the kitchen where everyone is too busy to worry about you.")
     print("You see a stash of high quality prime meat. Do you steal some meat?")
@@ -106,14 +110,15 @@ def kitchen():
 
     if (choice == "yes") and HaveMeat:
         print("You are already carrying some of the stolen meat.")
-        print("You leave the kitchen.")
+        print("You leave the kitchen.\n")
         cantina()
     elif (choice == "yes") and (HaveMeat == False):
-        print("You quickly steal some meat, put it in your coat, and rush out the door.")
+        print("You quickly steal some meat, put it in your coat, and rush out the door.\n")
         HaveMeat = True
         cantina()
     else:
-        print("You take a look around and decide to leave.")
+        print("You take a look around and decide to leave.\n")
         cantina()
 
+print("You are drunk from partying and you are standing outside in a sandy plaza on Tatooine.\n")
 outside()
